6581e73ea477eb73670483ab7e682ab610bc20e3
MohammedAyman2018 <emidobibo20@gmail.com>
2020-09-17 17:07:07 +0200

    Fix navbar

--- components/mainNavbar.vue
+++ components/mainNavbar.vue
@@ -5,15 +5,17 @@
         Lend2friend
       </v-toolbar-title>
       <v-spacer />
-      <v-btn v-if="$auth.loggedIn" text to="/admin/">
-        Admin panel
-      </v-btn>
       <v-btn v-if="!$auth.loggedIn" text to="/login">
         login
       </v-btn>
-      <v-btn v-else color="danger" @click="$auth.logout('local')">
-        logout
-      </v-btn>
+      <div v-else>
+        <v-btn text to="/admin/">
+          Admin panel
+        </v-btn>
+        <v-btn color="danger" @click="$auth.logout('local')">
+          logout
+        </v-btn>
+      </div>
     </v-app-bar>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,31 @@
 <template>
-  <v-app>
-    <!-- <main-navbar /> -->
-    <div>
+  <div>
+    <v-app-bar dense dark>
+      <v-toolbar-title>
+        Lend2friend
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn v-if="!$auth.loggedIn" text to="/login">
+        login
+      </v-btn>
+      <div v-else>
+        <v-btn text to="/admin/">
+          Admin panel
+        </v-btn>
+        <v-btn color="danger" @click="$auth.logout('local')">
+          logout
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-app>
       <nuxt />
-    </div>
-  </v-app>
+    </v-app>
+  </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
